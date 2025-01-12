27aa2e70305798e2c55e4a4be7fb0e2c5b019e0c
Vaidik Shreshth <vaidikjsk@gmail.com>
2025-01-13 00:25:28 +0530

    solved vercel error

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -2,12 +2,12 @@ export interface simpleBlogCard {
   title: string;
   smallDescription: string;
   currentSlug: string;
-  titleImage: { asset: { _ref: string } }; // Replace any with a more specific type
+  titleImage: any;
 }
 
 export interface fullBlog {
   currentSlug: string;
   title: string;
-  content: string | { [key: string]: any }; // Replace any with a more specific type or a complex object
-  titleImage: { asset: { _ref: string } }; // Replace any with a more specific type
+  content: any;
+  titleImage: any;
 }
--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -2,15 +2,14 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  apiVersion: "2022-03-07",
+  apiVersion: "2023-05-03",
   dataset: "production",
-  projectId: "jrdr77q9",
+  projectId: "sw0fgp2s",
   useCdn: false,
 });
 
 const builder = imageUrlBuilder(client);
 
-// Define a specific type for the source parameter
-export function urlFor(source: string | { asset: { _ref: string } }): string {
-  return builder.image(source).url(); // Ensure you call .url() to get the URL as a string
+export function urlFor(source: any) {
+  return builder.image(source);
 }
